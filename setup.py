4db384143e4cbd2250a0d855e4005a01a3d9aa3d
Matthew Cazaly <15869779+Fakas@users.noreply.github.com>
2020-05-27 10:04:24 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="Example Python Package",
+    version="1.0.3",
+    description="An example Python package",
+    url="https://example.com",
+    author="matcaz",
+    packages=["example"],
+    include_package_data=True,
+)
