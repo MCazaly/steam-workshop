6a9240ff8cc67662a9aed5a75f8e9a1e55086e1d
Fakas <matthewcaz@gmail.com>
2020-05-27 13:05:36 +0100

    Fix package name...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name="Example Python Package",
+    name="steam-workshop",
     version="0.0.1",
     description="A package to scrape Collection information from the Steam Workshop.",
     url="https://github.com/Fakas/steam-workshop",
